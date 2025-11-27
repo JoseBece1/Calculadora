6a1a8b4bf5bfb7042532337458be47b520bdc58d
José B <jose.bece.r2@gmail.com>
2025-11-27 12:56:06 -0600

    Creacion del menu

--- main.c
+++ main.c
@@ -1,9 +1,15 @@
 #include "stdio.h"
 
 int main(){
-    int opc = 0;
-    do{
-    printf("--------Menu--------}\n");
-    scanf("%d", &opc);
+    int opc;
+    do {
+        printf("\n====== Calculadora Simple ======\n");
+        printf("1) Sumar\n");
+        printf("2) Restar\n");
+        printf("3) Multiplicar\n");
+        printf("4) Dividir\n");
+        printf("0) Salir\n");
+        printf("Seleccione una opción: ");
+        scanf("%d", &opc);
     }while(opc != 0);
 }
