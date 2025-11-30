0a8c2bc442f0791eab9d1db5642ebf9c8bec9745
GabrielVazquez420 <a21300731@ceti.mx>
2025-11-30 13:21:20 -0600

    Agregue lo de division

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 
 void suma();
 void resta();
+void multiplicar();
+void dividir();
 
 int main(){
     int opc;
@@ -20,8 +22,11 @@ int main(){
         if (opc == 2){
             resta();
         }
-         if (opc == 3){
+        if (opc == 3){
             multiplicar();
+        }
+        if (opc == 4){
+            dividir();
         }
     }while(opc != 0);
 }
@@ -51,4 +56,18 @@ void multiplicar(){
     printf("Ingresa el segundo número: ");
     scanf("%f", &b);
     printf("Resultado: %.2f\n", a * b);
+}
+
+void dividir(){
+    float a, b;
+    printf("Ingresa el primer número: ");
+    scanf("%f", &a);
+    printf("Ingresa el segundo número: ");
+    scanf("%f", &b);
+    
+    if (b != 0) {
+        printf("Resultado: %.2f\n", a / b);
+    } else {
+        printf("Error: No se puede dividir entre cero\n");
+    }
 }
